f4e7e493eafa55b9b9ac315c60cd8c9ec783859c
ayoub99a <ayuob1999118a@gmail.com>
2022-10-24 14:54:46 +0300

    Done -> Create, Read, Delete

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 // DONE getTotal
 // DONE createProduct
-// ! saveInLocalstorage
-// ! EmptyFields
-// ! showInPage
+// DONE saveInLocalstorage
+// DONE EmptyFields
+// DONE showInPage
 // ! delete
 // ! update
 // ! search
@@ -19,19 +19,20 @@ let category = document.getElementById("category");
 let createBtn = document.getElementById("createBtn");
 let productsContainer = document.getElementById("productsContainer");
 
-function calcTotal(total) {
-  if (productName.value && price.value) {
-    let totalValue = +price.value + +tax.value + +ads.value - +discount.value;
-    total.innerHTML = totalValue;
-    total.style.backgroundColor = "#4caf50";
-  } else {
-    total.innerHTML = "";
-    total.style.backgroundColor = "rgb(255, 0, 0, 0.5)";
-  }
-}
-
 let requiredInputs = [productName, price, tax, ads, discount];
+let allInputs = [productName, price, tax, ads, discount, count, category];
+let allInputsAndTotal = [
+  productName,
+  price,
+  tax,
+  ads,
+  discount,
+  total,
+  count,
+  category,
+];
 
+// Calculate Total
 requiredInputs.forEach((input) => {
   input.addEventListener("input", function () {
     calcTotal(total);
@@ -41,6 +42,7 @@ requiredInputs.forEach((input) => {
 // Create an Empty Array
 let productsArr = [];
 
+// EVENTS
 // Get from localstorage if exist
 window.addEventListener("load", function () {
   if (this.localStorage.getItem("allProducts")) {
@@ -56,6 +58,17 @@ createBtn.addEventListener("click", function (e) {
   e.preventDefault();
   addProduct(productName, price, tax, ads, discount, count, category);
 });
+
+function calcTotal(total) {
+  if (productName.value && price.value) {
+    let totalValue = +price.value + +tax.value + +ads.value - +discount.value;
+    total.innerHTML = totalValue;
+    total.style.backgroundColor = "#4caf50";
+  } else {
+    total.innerHTML = "";
+    total.style.backgroundColor = "rgb(255, 0, 0, 0.5)";
+  }
+}
 
 function addProduct(name, price, tax, ads, discount, count, category) {
   // Check if There are The REQUIRED Values.
@@ -79,10 +92,17 @@ function addProduct(name, price, tax, ads, discount, count, category) {
       count: count.value || 1,
       category: category.value,
     };
+    // Add Product To Array
     productsArr.push(obj);
+    // Add Product To Page
     addToPage(obj);
+    // Add Product To Localstorage
     addToLocalstorge(productsArr);
-    clearInputs([requiredInputs, count, category]);
+    // EMPTY the Input fileds
+    clearInputs(allInputs);
+
+    // Re-Focus to Product Name Filed
+    name.focus();
   } else {
     console.log("ERROR");
   }
@@ -96,16 +116,55 @@ function addToPage(obj) {
     dataEle.innerHTML = Object.values(obj)[i];
     productRow.append(dataEle);
   }
-  let icons = ["./imgs/update.png", "./imgs/delete.webp"];
-  for (let i = 0; i < icons.length; i++) {
-    let iconTd = document.createElement("td");
-    let icon = document.createElement("img");
-    icon.className = "icon";
-    icon.src = icons[i];
-    iconTd.append(icon);
-    productRow.append(iconTd);
-  }
+  // Set class for ID Fields
+
+  // Update Icon
+  let updateTd = document.createElement("td");
+  let updateIcon = document.createElement("img");
+  updateIcon.className = "icon updateIcon";
+  updateIcon.src = "./imgs/update.png";
+  updateTd.append(updateIcon);
+  productRow.append(updateTd);
+
+  // Update Icon FUNCTION
+  updateTd.addEventListener("click", function (e) {
+    productName.focus();
+    let targetProduct = e.target.parentElement.parentElement;
+    console.log(targetProduct);
+    // console.log(targetProduct.children[1].innerHTML);
+    // TODO Working Correctly but STATIC.
+    // productName.value = targetProduct.children[1].innerHTML;
+    let targetChildren = targetProduct.children;
+    for (let i = 1; i < targetChildren.length - 2; i++) {
+      if (allInputsAndTotal[i - 1].nodeName === "INPUT") {
+        allInputsAndTotal[i - 1].value = targetChildren[i].innerHTML;
+      } else {
+        // allInputsAndTotal[i - 1].innerHTML = targetChildren[i].innerHTML;
+        calcTotal(total)
+      }
+    }
+    createBtn.innerHTML = "Update";
+    createBtn.addEventListener("click", function () {
+      removeFromPage(e);
+      removeFromArr(e);
+      createBtn.innerHTML = "Create";
+    });
+  });
+
+  // Delete Icon
+  let deleteTd = document.createElement("td");
+  let deleteIcon = document.createElement("img");
+  deleteIcon.className = "icon deleteIcon";
+  deleteIcon.src = "./imgs/delete.webp";
+  deleteTd.append(deleteIcon);
+  productRow.append(deleteTd);
   productsContainer.append(productRow);
+
+  // Delete Icon FUNCTION
+  deleteIcon.addEventListener("click", function (e) {
+    removeFromPage(e);
+    removeFromArr(e);
+  });
 }
 
 function addToLocalstorge(arr) {
@@ -114,12 +173,128 @@ function addToLocalstorge(arr) {
 
 function clearInputs(inputs) {
   inputs.forEach((input) => {
-    if (Array.isArray(input)) {
-      input.forEach(singleInput => {
-        singleInput.value = "";
-      })
-    } else {
-      input.value = ""
-    }
+    input.value = "";
   });
 }
+
+function removeFromPage(e) {
+  e.target.parentElement.parentElement.remove();
+  // Renumbering in PAGE
+  for (let i = 0; i < productsContainer.children.length; i++) {
+    productsContainer.children[i].children[0].innerHTML = i + 1;
+  }
+}
+
+function removeFromArr(e) {
+  let theDeletedId = parseInt(
+    e.target.parentElement.parentElement.firstChild.innerHTML
+  );
+  for (let i = 0; i < productsArr.length; i++) {
+    // Remove From Array
+    if (productsArr[i].id === theDeletedId) {
+      productsArr.splice(i, 1);
+      console.log(productsArr);
+    }
+    // Renumbering the Array
+    if (productsArr[i]) {
+      productsArr[i].id = i + 1;
+    }
+    // Add array AFTER UPDATE.
+    addToLocalstorge(productsArr);
+  }
+}
+
+// !Automatic ADD => For Testing
+
+// productsArr = [
+//   {
+//     id: 1,
+//     name: "Iphone 11 Pro",
+//     price: "900",
+//     tax: "0",
+//     ads: "5",
+//     discount: "10",
+//     total: "895",
+//     count: "1",
+//     category: "Phone",
+//   },
+//   {
+//     id: 2,
+//     name: "Realme Q3 Pro",
+//     price: "350",
+//     tax: "0",
+//     ads: "2",
+//     discount: "5",
+//     total: "347",
+//     count: 1,
+//     category: "Phone",
+//   },
+//   {
+//     id: 3,
+//     name: "Iphone 11 Pro",
+//     price: "1200",
+//     tax: "0",
+//     ads: "10",
+//     discount: "10",
+//     total: "1200",
+//     count: "1",
+//     category: "Phone",
+//   },
+//   {
+//     id: 4,
+//     name: "Iphone 13 Pro",
+//     price: "1300",
+//     tax: "0",
+//     ads: "10",
+//     discount: "5",
+//     total: "1305",
+//     count: "2",
+//     category: "Phone",
+//   },
+//   {
+//     id: 5,
+//     name: "Iphone 7",
+//     price: "500",
+//     tax: "0",
+//     ads: "0",
+//     discount: "0",
+//     total: "500",
+//     count: 1,
+//     category: "Phone",
+//   },
+//   {
+//     id: 6,
+//     name: "Lenovo Ideapad 510",
+//     price: "605",
+//     tax: "0",
+//     ads: "2",
+//     discount: "2",
+//     total: "605",
+//     count: 1,
+//     category: "Laptop",
+//   },
+//   {
+//     id: 7,
+//     name: "Huawei P10",
+//     price: "410",
+//     tax: "0",
+//     ads: "0",
+//     discount: "0",
+//     total: "410",
+//     count: 1,
+//     category: "Phone",
+//   },
+//   {
+//     id: 8,
+//     name: "Samsung Tab s7fe",
+//     price: "400",
+//     tax: "0",
+//     ads: "10",
+//     discount: "10",
+//     total: "400",
+//     count: 1,
+//     category: "Tablet",
+//   },
+// ];
+
+// addToLocalstorge(productsArr);
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 # Add Project Files.
 
 ## Done HTML, CSS Part
+
+## DONE Create, Read, Detete 
